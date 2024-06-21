cf3ed9fb4c1c852255207724c055df4e8ff9c022
Dominic Petriella <dpetriella@magicwrighter.com>
2024-06-21 10:52:00 -0400

    added error handling to creating a room code

--- lib/app/db_functions/room/room_functions.dart
+++ lib/app/db_functions/room/room_functions.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kick_flip/app/models/room.dart';
 import 'package:kick_flip/app/state/current_game_state.dart';
 import 'package:kick_flip/main.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:uuid/uuid.dart';
 
 String _createRoomCode() {
@@ -12,11 +13,27 @@ String _createRoomCode() {
 
 Future createNewRoom(WidgetRef ref) async {
   String roomCode = _createRoomCode();
-  var response = await supabase
-      .from('rooms')
-      .insert({'room_code': roomCode})
-      .select()
-      .count();
-  var data = Room.fromJson(response.data[0]);
-  ref.read(currentGameStateProvider.notifier).setCurrentRoom(data);
+  PostgrestResponse<List<Map<String, dynamic>>>? response;
+  try {
+    response = await supabase
+        .from('rooms')
+        .insert({'room_code': roomCode})
+        .select()
+        .count();
+  } catch (e) {
+    print('Failure to reach db, Error: ${e.toString()}');
+  }
+
+  if (response != null) {
+    Map<String, dynamic>? data = response.data.firstOrNull;
+    if (data != null) {
+      ref
+          .read(currentGameStateProvider.notifier)
+          .setCurrentRoom(Room.fromJson(data));
+    } else {
+      print("Data as list returned from db as list of NULL");
+    }
+  } else {
+    print('Response return null- No tables returned');
+  }
 }
